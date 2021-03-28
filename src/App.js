eb00cd183ed55e91286974a82e74c78ddc3a36fe
Anni Maasik <amaasik@gmail.com>
2021-03-28 14:51:29 +0300

    ostukorvi rakendus

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import Cart from './Cart';
 import './App.css';
+
+const items = [
+  { id: 1, name: 'Maasikad', price: 2.25, qty: 2},
+  { id: 2, name: 'Mustikad', price: 1.99, qty: 3},
+  { id: 3, name: 'Vaarikad', price: 3.50, qty: 1},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <Cart initialItems={items} />
+   </div>
   );
 }
 
